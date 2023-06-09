603e4ce59541113ffc4289ce0e3527bdc98fd488
Allan Fang <32990185+allanfang1@users.noreply.github.com>
2023-06-09 16:23:27 -0400

    Fixed requested changes
    Camel to snake case
    Padding start/end instead of left/right

--- app/src/main/java/com/allan/boardbuddies/EditActivity.java
+++ app/src/main/java/com/allan/boardbuddies/EditActivity.java
@@ -14,11 +14,11 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.edit_note_toolbar);
         toolbar.setNavigationOnClickListener(v -> onBackPressed());
 
-        View extraView = findViewById(R.id.remainingScrollSpace);
-        EditText editTextNoteContent = findViewById(R.id.editTextNoteContent);
+        View extraView = findViewById(R.id.extra_scrollspace);
+        EditText editTextNoteContent = findViewById(R.id.edit_textnote_content);
         extraView.setOnClickListener(v -> editTextNoteContent.requestFocus());
 
     }
