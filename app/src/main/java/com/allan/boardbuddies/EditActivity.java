45e75b9dd07d3a55ea59832ff5d67e37b9d4752c
Allan Fang <32990185+allanfang1@users.noreply.github.com>
2023-06-09 23:38:12 -0400

    Saves files to local as JSON
    Reloads all local data on resume to notes.
    Next steps: Add delete functionality, add open note

--- app/src/main/java/com/allan/boardbuddies/EditActivity.java
+++ app/src/main/java/com/allan/boardbuddies/EditActivity.java
@@ -2,11 +2,14 @@ package com.allan.boardbuddies;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ScrollView;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -14,12 +17,39 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.edit_note_toolbar);
-        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+        Toolbar toolbar = findViewById(R.id.edit_note_toolbar);
+        toolbar.setNavigationOnClickListener(v -> {
+            saveTextNote();
+            onBackPressed();
+        });
 
         View extraView = findViewById(R.id.extra_scrollspace);
         EditText editTextNoteContent = findViewById(R.id.edit_textnote_content);
         extraView.setOnClickListener(v -> editTextNoteContent.requestFocus());
 
     }
+
+    private void saveTextNote(){
+        EditText editText1 = findViewById(R.id.edit_textnote_title);
+        EditText editText2 = findViewById(R.id.edit_textnote_content);
+        String title = editText1.getText().toString();
+        String content = editText2.getText().toString();
+        if (!title.trim().isEmpty() || !content.trim().isEmpty()){
+            String fileName = System.currentTimeMillis() + ".json";
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("title", title);
+                jsonObject.put("content", content);
+                String jsonString = jsonObject.toString();
+
+                File file = new File(getFilesDir(), fileName);
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write(jsonString);
+                fileWriter.close();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/allan/boardbuddies/fragments/NotesFragment.java
+++ app/src/main/java/com/allan/boardbuddies/fragments/NotesFragment.java
@@ -15,19 +15,22 @@ import com.allan.boardbuddies.Note;
 import com.allan.boardbuddies.NoteAdapter;
 import com.allan.boardbuddies.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- *
- */
 public class NotesFragment extends Fragment {
     private ArrayList<Note> notes = new ArrayList<Note>();
     private RecyclerView recyclerView;
     private FloatingActionButton addNoteFab;
     private NoteAdapter adapter;
+
     public NotesFragment() {
         // Required empty public constructor
     }
@@ -36,6 +39,7 @@ public class NotesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = getArguments();
+        loadNotes();
         if (bundle != null) {
             notes = (ArrayList<Note>)bundle.getSerializable("myNotes");
             // Handle the retrieved data as needed
@@ -67,5 +71,40 @@ public class NotesFragment extends Fragment {
                 view.getContext().startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadNotes();
+        adapter.notifyDataSetChanged();
+    }
+
+    private void loadNotes(){
+        File[] files = requireContext().getFilesDir().listFiles();
+        notes.clear();
+        for (File file : files){
+            if (file.isFile() && file.getName().endsWith(".json")) {
+                try {
+                    // Read the file contents
+                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    reader.close();
+
+                    // Parse the JSON content
+                    String jsonContent = stringBuilder.toString();
+                    JSONObject jsonObject = new JSONObject(jsonContent);
+
+                    // Add the parsed JSON content to the list
+                    notes.add(new Note((String) jsonObject.get("title"), (String) jsonObject.get("content")));
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
